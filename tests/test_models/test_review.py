dbb451e5dd5c52a81bd325dab9ba9d3b3a7afeb9
Amna2011 <amnataha2011@gmail.com>
2023-09-17 09:45:45 -0700

    mmm

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -9,22 +9,22 @@ class test_review(test_basemodel):
     """my review function test """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """mu init fun """
         super().__init__(*args, **kwargs)
         self.name = "Review"
         self.value = Review
 
     def test_place_id(self):
-        """ """
+        """my place id function """
         new = self.value()
         self.assertEqual(type(new.place_id), str)
 
     def test_user_id(self):
-        """ """
+        """user id func test """
         new = self.value()
         self.assertEqual(type(new.user_id), str)
 
     def test_text(self):
-        """ """
+        """test test func """
         new = self.value()
         self.assertEqual(type(new.text), str)
