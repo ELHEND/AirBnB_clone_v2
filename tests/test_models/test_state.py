00d44eb2c76421f9e0c960c25ea9ae50d050872a
Amna2011 <amnataha2011@gmail.com>
2023-09-17 09:49:38 -0700

    mmm

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -9,12 +9,12 @@ class test_state(test_basemodel):
     """state function test """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """my init """
         super().__init__(*args, **kwargs)
         self.name = "State"
         self.value = State
 
     def test_name3(self):
-        """ """
+        """my test name3 func """
         new = self.value()
         self.assertEqual(type(new.name), str)
