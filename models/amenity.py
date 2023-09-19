926af2b61dd7ebdc8601f5bb96e94016d0355bc3
ELHEND <alhnd683@gmail.com>
2023-09-18 22:54:38 -0700

    amnahend

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """This is  amenity class"""
-<<<<<<< HEAD
 
-=======
->>>>>>> c5cc3f3369e8c3452879eee63c7f95a92f72757b
 from models.base_model import BaseModel, Base
+
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 
