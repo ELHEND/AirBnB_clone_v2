43269f50a1330232c3bdd3a4b7aeb7758e2315eb
ELHEND <alhnd683@gmail.com>
2023-10-06 01:49:04 -0700

    w

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,42 +1,49 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
-from fabric.api import *
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
+
+env.hosts = ["104.196.168.90", "35.196.46.172"]
+
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to the web servers.
+    """Distributes an archive to a web server.
 
     Args:
-        archive_path: The path to the archive file.
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    # Upload the archive file to the `/tmp/` directory of the web server.
-    put(archive_path, '/tmp/')
-
-    # Uncompress the archive file to the folder `/data/web_static/releases/<archive filename without extension>` on the web server.
-    run('mkdir -p /data/web_static/releases/{}/'.format(os.path.basename(archive_path).split('.')[0]))
-    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(os.path.basename(archive_path), os.path.basename(archive_path).split('.')[0]))
-
-    # Delete the archive file from the web server.
-    run('rm /tmp/{}'.format(os.path.basename(archive_path)))
-
-    # Delete the symbolic link `/data/web_static/current` from the web server.
-    run('rm -rf /data/web_static/current')
-
-    # Create a new the symbolic link `/data/web_static/current` on the web server, linked to the new version of your code (`/data/web_static/releases/<archive filename without extension>`).
-    run('ln -s /data/web_static/releases/{} /data/web_static/current'.format(os.path.basename(archive_path).split('.')[0]))
-
-@task
-def deploy():
-    """
-    Deploys the web application to the web servers.
-    """
-
-    # Get the path to the archive file.
-    archive_path = 'versions/web_static_20170315003959.tgz'
-
-    # Deploy the web application to the web servers.
-    do_deploy(archive_path)
-
-env.hosts = ['52.55.249.213', '54.157.32.137']
-
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
