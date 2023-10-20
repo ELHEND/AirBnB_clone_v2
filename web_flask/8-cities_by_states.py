6acb11ccad78843d250dbce637b41e5b95536ab3
ELHEND <alhnd683@gmail.com>
2023-10-20 00:55:57 -0700

    hendae

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""Starts Flask web application.
 
-The application listens on 0.0.0.0, port 5000.
+application listens on 0.0.0.0, port 5000.
 Routes:
     /cities_by_states: HTML page with a list of all states and related cities.
 """
@@ -24,7 +24,7 @@ def cities_by_states():
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """Remove  current SQLAlchemy session."""
     storage.close()
 
 
