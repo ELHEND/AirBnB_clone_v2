e349d0a4f7175255c6a0483b8a32fa7307d2e2b5
ELHEND <alhnd683@gmail.com>
2023-10-20 00:59:09 -0700

    hend

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""Starts  Flask web application.
 
-The application listens on 0.0.0.0, port 5000.
+application listens on 0.0.0.0, port 5000.
 Routes:
     /states: HTML page with a list of all State objects.
     /states/<id>: HTML page displaying the given state with <id>.
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route("/states", strict_slashes=False)
 def states():
-    """Displays an HTML page with a list of all States.
+    """Displays HTML page with a list of all States.
 
     States are sorted by name.
     """
@@ -25,7 +25,7 @@ def states():
 
 @app.route("/states/<id>", strict_slashes=False)
 def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
+    """Displays  HTML page with info about <id>, if it exists."""
     for state in storage.all("State").values():
         if state.id == id:
             return render_template("9-states.html", state=state)
